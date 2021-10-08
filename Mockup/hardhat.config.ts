efaaf83a870e4a88e2cabc76d0438be18540b60a
gympohnpimol <gympohnpimol@gmail.com>
2021-10-08 23:55:46 +0700

    node connect :art:

---
+++ Mockup/hardhat.config.ts
@@ -0,0 +1,38 @@
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig } from "hardhat/config";
+
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+const mnemonic = '';
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: '',
+      accounts: {mnemonic: mnemonic}
+    }
+  },
+  typechain: {
+    outDir: 'src/types',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  },
+} as HardhatUserConfig;
